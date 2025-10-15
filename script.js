62e36a7f656fb4ca471bdcd5209f89f030d37dd1
Rakshit5594 <111284464+Rakshit5594@users.noreply.github.com>
2025-10-15 22:35:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
   const ctx = canvas.getContext('2d');
 
   const opts = {
-    strings: ["HAPPY", "DIWALI TO ALL "],   // change text here
+    strings: ["HAPPY", "DIWALI  "],   // change text here
     charSize: 44,
     charSpacing: 62,
     lineHeight: 72,
